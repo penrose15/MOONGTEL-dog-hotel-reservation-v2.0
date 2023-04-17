ae437a44c67c54464f0093ff8cced2ec67d0e18f
penrose15 <seongju99@naver.com>
2023-04-18 02:51:52 +0900

    fix : fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,9 +14,9 @@ else
 fi
 
 #cp build/libs/reservation-0.0.1-SNAPSHOT.jar /home/ec2-user/reservation
-echo "nohup java -jar -Dspring.config.location=classpath:/${real},/home/ec2-user/app/application-db.yml /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &"
+echo "nohup java -jar -Dspring.config.location=src/main/resources/${real},/home/ec2-user/app/application-db.yml /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &"
 java -jar \
-  -Dspring.config.location=classpath:/${real},/home/ec2-user/app/application-db.yml \
+  -Dspring.config.location=${real},/home/ec2-user/app/application-db.yml \
    /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar
 
 for retry_count in $(seq 10)
