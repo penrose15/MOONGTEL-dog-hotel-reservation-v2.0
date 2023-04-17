88e0ad99348c1e21a487a21307bbb8e6dfbf31c1
penrose15 <seongju99@naver.com>
2023-04-18 03:11:55 +0900

    fix : fix deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ green_port=8081
 if curl -s "http://localhost:${blue_port}" > /dev/null # 서버가 살아있으면
 then
     deployment_target=${blue_port}
-		real="real1"
+		real="application-real1.yml"
 else
     deployment_target=${green_port}
-		real="real2"
+		real="application-real2.yml"
 fi
 
 #cp build/libs/reservation-0.0.1-SNAPSHOT.jar /home/ec2-user/reservation
-echo "nohup java -jar -Dspring.profiles.active=${real},/home/ec2-user/app/application-db.yml /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &"
+echo "nohup java -jar -Dspring.config.location=/home/ec2-user/app/${real},/home/ec2-user/app/application-db.yml /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &"
 java -jar \
   -Dspring.config.location=${real},/home/ec2-user/app/application-db.yml \
    /home/ec2-user/reservation/reservation-0.0.1-SNAPSHOT.jar
