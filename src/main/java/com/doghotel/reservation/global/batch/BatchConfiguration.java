0081fccd5ea95a3077bcd080f643400f6d9a7062
penrose15 <seongju99@naver.com>
2023-04-17 22:34:51 +0900

    test : fix tests

--- src/main/java/com/doghotel/reservation/global/batch/BatchConfiguration.java
+++ src/main/java/com/doghotel/reservation/global/batch/BatchConfiguration.java
@@ -1,24 +1,37 @@
 package com.doghotel.reservation.global.batch;
 
+import com.doghotel.reservation.domain.post.entity.Posts;
 import com.doghotel.reservation.domain.post.entity.PostsScore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ItemWriteListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.configuration.annotation.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.security.core.parameters.P;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,52 +42,45 @@ public class BatchConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final EntityManagerFactory entityManagerFactory;
+    private final DataSource dataSource;
+
+    private static final int chunkSize = 10;
+
+    //"select p from PostsScore p group by p.posts.id"
 
     @Bean
-    public Job testJob() {
-        return jobBuilderFactory.get("test")
-                .start(testStep()).build();
-    }
-
-    @Bean
-    @JobScope
-    public Step testStep() {
-        return stepBuilderFactory.get("testStep")
-                .<PostsScore, PostsScore> chunk(10)
-                .reader(reader(null))
-                .processor(processor(null))
-                .writer(writer(null))
+    public Job jdbcCursorItemReaderJob() {
+        return jobBuilderFactory.get("jdbcCursorItemReaderJob")
+                .start(jdbcCursorItemReaderStep())
                 .build();
     }
 
     @Bean
-    @StepScope
-    public JpaPagingItemReader <PostsScore> reader(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        log.info("reader value  : " + requestDate);
-
-//        Map<String, Object> parameterValues = new HashMap<>();
-        return new JpaPagingItemReaderBuilder<PostsScore>()
-                .pageSize(10)
-                .queryString("select p from PostsScore p group by p.posts.id")
-                .entityManagerFactory(entityManagerFactory)
-                .name("JpaPagingItemReader")
+    public Step jdbcCursorItemReaderStep() {
+        return stepBuilderFactory.get("jdbcCursorItemReaderStep")
+                .<PostsScore, PostsScore>chunk(chunkSize)
+                .reader(jdbcCursorItemReader())
+                .writer(jdbcCursorItemWriter())
                 .build();
     }
 
     @Bean
-    @StepScope
-    public ItemProcessor<PostsScore, PostsScore> processor(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return item -> {
-            item.calculateScore();
-            return item;
+    public JdbcCursorItemReader<PostsScore> jdbcCursorItemReader() {
+        return new JdbcCursorItemReaderBuilder<PostsScore>()
+                .fetchSize(chunkSize)
+                .dataSource(dataSource)
+                .rowMapper(new BeanPropertyRowMapper<>(PostsScore.class))
+                .sql("select * from PostsScore")
+                .name("jdbcCursorItemReader")
+                .build();
+    }
+
+    private ItemWriter<PostsScore> jdbcCursorItemWriter() {
+        return list -> {
+            for(PostsScore p : list) {
+                p.calculateScore();
+                log.info("p.avg={}",p.getScore());
+            }
         };
-    }
-
-    @Bean
-    @StepScope
-    public JpaItemWriter<PostsScore> writer(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return new JpaItemWriterBuilder<PostsScore>()
-                .entityManagerFactory(entityManagerFactory)
-                .build();
     }
 }
--- src/test/java/com/doghotel/reservation/review/service/ReviewServiceTest.java
+++ src/test/java/com/doghotel/reservation/review/service/ReviewServiceTest.java
@@ -1,4 +1,120 @@
 package com.doghotel.reservation.review.service;
 
+import com.doghotel.reservation.domain.company.entity.Company;
+import com.doghotel.reservation.domain.company.repository.CompanyRepository;
+import com.doghotel.reservation.domain.customer.entity.Customer;
+import com.doghotel.reservation.domain.customer.repository.CustomerRepository;
+import com.doghotel.reservation.domain.reservation.entity.Reservation;
+import com.doghotel.reservation.domain.reservation.entity.ReservedDogs;
+import com.doghotel.reservation.domain.reservation.repository.ReservationRepository;
+import com.doghotel.reservation.domain.reservation.repository.ReservedDogIdRepository;
+import com.doghotel.reservation.domain.review.entity.Review;
+import com.doghotel.reservation.domain.review.repository.ReviewImgRepository;
+import com.doghotel.reservation.domain.review.repository.ReviewRepository;
+import com.doghotel.reservation.domain.review.repository.ReviewRepositoryImpl;
+import com.doghotel.reservation.domain.room.entity.Room;
+import com.doghotel.reservation.domain.room.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 public class ReviewServiceTest {
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private ReviewImgRepository reviewImgRepository;
+    @Autowired
+    private ReviewRepositoryImpl reviewRepositoryImpl;
+    @Autowired
+    private ReservationRepository reservationRepository;
+    @Autowired
+    private ReservedDogIdRepository reservedDogIdRepository;
+    @Autowired
+    private CompanyRepository companyRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+
+
+    private Customer customer;
+    private Company company;
+    private Reservation reservation;
+    private Room room;
+    private Review review;
+    @BeforeEach
+    public void init() {
+
+        customer = Customer.builder()
+                .username("name")
+                .email("customer@gmail.com")
+                .password("password1234!")
+                .phone("010-1111-2222")
+                .build();
+        customer = customerRepository.save(customer);
+        company = Company.builder()
+                .email("company@gmail.com")
+                .password("1234abcd!")
+                .companyName("test company")
+                .address("test address")
+                .detailAddress("test detailAddress")
+                .representativeNumber("123456789")
+                .build();
+        company = companyRepository.save(company);
+
+        room = Room.builder()
+                .price(10000)
+                .roomCount(10)
+                .roomSize("for small dog")
+                .company(company)
+                .build();
+        room = roomRepository.save(room);
+
+        reservation = Reservation.builder()
+                .room(room)
+                .checkInDate(LocalDate.of(2024,02,03))
+                .checkOutDate(LocalDate.of(2024,02,06))
+                .dogCount(5)
+                .totalPrice(100000)
+                .customer(customer)
+                .company(company)
+                .build();
+        reservation = reservationRepository.save(reservation);
+
+        ReservedDogs reservedDogs = ReservedDogs.builder()
+                .dogId(1L)
+                .reservation(reservation)
+                .build();
+        ReservedDogs reservedDogs1 = ReservedDogs.builder()
+                .dogId(1L)
+                .reservation(reservation)
+                .build();
+        reservedDogIdRepository.save(reservedDogs);
+        reservedDogIdRepository.save(reservedDogs1);
+
+        review = Review.builder()
+                .title("title")
+                .company(company)
+                .content("content")
+                .customer(customer)
+                .score(0.5)
+                .build();
+        review = reviewRepository.save(review);
+    }
+
+    @Test
+    void findByReservationIdTest() {
+        Optional<Review> optional = reviewRepository.findByReservationId(reservation.getReservationId());
+        assertThat(optional)
+                .isPresent();
+    }
 }
